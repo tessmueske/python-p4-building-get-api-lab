c315c2ab6859c859009923c9475581600567b1ba
Tess <tess.mueske@gmail.com>
2024-08-30 17:26:25 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,52 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+
+    return make_response(
+        jsonify(bakery_dict), 
+        200
+    )
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [bg.to_dict() for bg in baked_goods]
+
+    response = make_response(
+        jsonify(baked_goods_list),
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).one_or_none()
+    baked_good_dict = baked_good.to_dict()
+
+    response = make_response(
+        jsonify(baked_good_dict),
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
